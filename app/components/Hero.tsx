732a15c09dd857974590282b9388e5b55ebf0cb2
masterpranay1 <rajpranaychottu@gmail.com>
2023-06-30 12:30:50 +0530

    Feat( Hero )

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,9 +1,58 @@
+import Image from 'next/image';
+import clsx from 'clsx';
+
+function Profile() {
+  return (
+      <div className={clsx(
+        'flex flex-row items-center justify-center gap-4',
+        'min-w-[50ch]'
+      )}>
+        <Image 
+          src="/images/profile.png"
+          alt="Picture of the testimony"
+          width={120}
+          height={120}
+        />
+
+        <div className='font-normal text-xl'>
+          <h3 className='font-medium text-2xl'>Simrin Sirur</h3>
+          <small className='leading-6 block my-4 text-xl'>Journalist <br/> The Print, Gurugram</small>
+          <p className='max-w-[45ch]'>"I was afraid to learn about investments and savings because it seemed to fly over my head, but Ankit was extremely patient and walked me through everything... I now feel more confident to handle my own money."</p>
+        </div>
+      </div>
+  )
+}
+
 export default function Hero() {
   return (
     <>
-      <main>
-        <h1>This is a hero</h1>
+      <main
+        className={clsx(
+          'flex flex-col justify-center items-center',
+          'bg-[#0A5783] text-white',
+          'p-12'
+        )}
+      >
+        <h2 className='text-4xl font-bold'>Transforming Lives - Financially!</h2>
+
+        <section className={clsx(
+          'flex flex-row items-center justify-center gap-4 flex-wrap',
+        )}>
+          <Profile />
+          <Image 
+            src="/images/arrow.png"
+            alt="arrow"
+            width={17}
+            height={34}
+          />
+          <Image
+            src="/images/hero-grid.png"
+            alt="hero"
+            width={456}
+            height={456}
+          />
+        </section>
       </main>
     </>
-  )
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <>
       <Header />
-      {/* <Hero /> */}
+      <Hero />
       {/* <Footer /> */}
     </>
   )
